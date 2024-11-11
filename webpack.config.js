6f6c656f2d8526c9065301af96c57210b1f23935
Siyu Li <lisy0526uk@gmail.com>
2024-11-11 14:42:29 +0000

    done with ZTM course

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackplugin({
+      template: './index.html',
+    }),
+  ],
+};
